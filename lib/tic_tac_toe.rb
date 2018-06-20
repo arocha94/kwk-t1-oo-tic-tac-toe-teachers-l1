718e78f71b18fb482bc59461e08ad9fe87539dee
Alejandra Rocha <arocha94@gmail.com>
2018-06-20 00:52:52 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -26,7 +26,7 @@ WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8
     end
   end
   
-  def position_taken?(board)
+  def position_taken?
     if @board[board] == "X" || @board[board] == "O"
       true
     else
